f17b6df9011e5cad65f32f90d4d708b6ec858ebc
zolcsika71 <bushdoctor2008@gmail.com>
2019-11-13 15:45:01 +0100

    classes

--- pidController.js
+++ pidController.js
@@ -1,129 +1,67 @@
-'use strict';
+function PID(p, i, d) {
+    this.sampleTime = 100;
+    this.target = 0;
+    this.output = 0;
 
-let classes = classes || {};
+    this.errorSum = 0;
+    this.lastInput = 0;
+    this.lastTime = Date.now() - this.sampleTime;
 
-classes.PidController = function () {
+    this.setTunings(p, i, d)
+}
 
-    let self = {},
-        privateScope = {};
+PID.prototype.setTunings = function(p, i, d) {
+    let ratio = this.sampleTime / 1000;
 
-    /**
-     *
-     * @type {number}
-     */
-    privateScope.target = null;
-
-    /**
-     *
-     * @type {{P: null, I: null, D: null}}
-     */
-    privateScope.gains = {
-        P: null,
-        I: null,
-        D: null
-    };
-
-    /**
-     *
-     * @type {number}
-     */
-    privateScope.Iterm = 0;
-
-    /**
-     *
-     * @type {{min: number, max: number}}
-     */
-    privateScope.ItermLimit = {
-        min: -1000,
-        max: 1000
-    };
-
-    /**
-     *
-     * @type {number}
-     */
-    privateScope.previousError = 0;
-
-    /**
-     *
-     * @type {{min: number, max: number, minThreshold: number}}
-     */
-    privateScope.output = {
-        min: null,
-        max: null,
-        minThreshold: null
-    };
-
-    /**
-     *
-     * @param {number} value
-     */
-    self.setTarget = function (value) {
-        privateScope.target = value;
-    };
-
-    /**
-     * @param {number} Pgain
-     * @param {number} Igain
-     * @param {number} Dgain
-     */
-    self.setGains = function (Pgain, Igain, Dgain) {
-        privateScope.gains.P = Pgain;
-        privateScope.gains.I = Igain;
-        privateScope.gains.D = Dgain;
-    };
-
-    /**
-     * Sets min and max value for output
-     * @param {number} min
-     * @param {number} max
-     * @param {number} minThreshold if output is below this value, [min] is returned
-     */
-    self.setOutput = function (min, max, minThreshold) {
-        privateScope.output.min = min;
-        privateScope.output.max = max;
-        privateScope.output.minThreshold = minThreshold;
-    };
-
-    /**
-     * Sets upper and lower limit for Iterm accumulator
-     * @param {number} min
-     * @param {number} max
-     */
-    self.setItermLimit = function (min, max) {
-        privateScope.ItermLimit.min = min;
-        privateScope.ItermLimit.max = max;
-    };
-
-    /**
-     * Executes PID controller based on current value and target
-     * @param {number} current
-     * @returns {number}
-     */
-    self.run = function (current) {
-        var error = current - privateScope.target,
-            Pterm = error * privateScope.gains.P,
-            Dterm = (error - privateScope.previousError) * privateScope.gains.D,
-            output;
-
-        privateScope.previousError = error;
-
-        privateScope.Iterm += error * privateScope.gains.I;
-        if (privateScope.Iterm > privateScope.ItermLimit.max) {
-            privateScope.Iterm = privateScope.ItermLimit.max;
-        } else if (privateScope.Iterm < privateScope.ItermLimit.min) {
-            privateScope.Iterm = privateScope.ItermLimit.min;
-        }
-
-        output = Pterm + privateScope.Iterm + Dterm;
-        if (output < privateScope.output.minThreshold) {
-            output = privateScope.output.min;
-        } else if (output > privateScope.output.max) {
-            output = privateScope.output.max;
-        }
-
-        return output;
-    };
-
-    return self;
+    this.p = p;
+    this.i = i * ratio;
+    this.d = d / ratio;
 };
+
+PID.prototype.setSampleTime = function(sampleTime) {
+    let ratio = sampleTime / this.sampleTime;
+
+    this.i *= ratio;
+    this.d /= ratio;
+    this.sampleTime = sampleTime;
+};
+
+PID.prototype.setOutputLimits = function(min, max) {
+    this.min = min;
+    this.max = max;
+};
+
+PID.prototype.setTarget = function(value) {
+    this.target = value
+};
+
+PID.prototype.compute = function(input) {
+    let now = Date.now(),
+        timeDiff = now - this.lastTime;
+
+    if (timeDiff >= this.sampleTime) {
+        let error = this.target - input,
+            inputDiff = input - this.lastInput;
+
+        this.errorSum = Math.max(this.min, Math.min(this.max, this.errorSum + (this.i * error)));
+        this.output = Math.max(this.min, Math.min(this.max, (this.p * error) + this.errorSum - (this.d * inputDiff)));
+        this.lastInput = input;
+        this.lastTime = now;
+    }
+
+    return this.output
+};
+
+let pid = new PID(1, 0.5, 2);
+pid.setSampleTime(1000);
+pid.setOutputLimits(0, 100);
+pid.setTarget(100);
+
+setInterval(function () {
+    let sign = Math.random(),
+        //input = sign < 0.5 ? Math.round(Math.random() * 90) : Math.round(Math.random() * 90) * -1,
+        processVariable = 10,
+        output = pid.compute(processVariable);
+
+    console.log(`in: ${processVariable} out: ${output}`);
+}, 500);
