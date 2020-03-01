0eff6cbbd040049b61d21c42754849ed25dd1509
zolcsika71 <bushdoctor2008@gmail.com>
2020-03-01 16:18:27 +0100

    asciii

--- ASCII/test.js
+++ ASCII/test.js
@@ -1,20 +1,54 @@
 "use strict";
 
+let str = [];
+str.push(" #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ");
+str.push("# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ");
+str.push("### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## ");
+str.push("# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ");
+str.push("# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  ");
 
-let str = " #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ";
+const
+    L_WIDTH = 4,
+    L_HEIGHT = 5,
+    TEXT = "E",
+    ABC_LENGTH = 27;
 
-//let str = "000 000 000 ";
-//let str = "Hello World!";
 
-for (let j = 0; j < str.length; j += 4) {
-    //console.error(`i: ${i}`);
-    //console.error(`j: ${j}`);
-    //let slice = str.slice(j, 4);
-    //console.error(`${str}`);
-    console.error(`${str.slice(j, j + 4)}`);
-    //console.error(`${str.slice(j, j + 4).length}`);
 
-    //letters.push(i, slice);
+let letters = [],
+    rowLength;
+
+function testLetters(letters) {
+    console.error(`${letters.length}`);
+    for (let i = 0; i < letters.length; i += L_WIDTH) {
+        //let lettersRow = letters[i];
+        //if (!rowLength)
+            //rowLength = lettersRow.length;
+        //console.error(`${lettersRow.length}`);
+
+        //console.error(`${i} ${j}`);
+        console.error(`${letters[i][L_WIDTH]}`);
+    }
 }
+
+function BB(x) {
+    return JSON.stringify(x, null, 2);
+}
+
+for (let i = 0; i < L_HEIGHT; i++) {
+
+    let row = str[i];
+
+    for (let j = 0; j < row.length; j += L_WIDTH) {
+        let slice = row.slice(j, j + L_WIDTH);
+        //console.error(`i: ${i} j: ${j} slice: ${slice}`);
+        letters.push(i, slice);
+    }
+}
+
+testLetters(letters);
+//console.error(`${BB(letters)}`);
+
+
 
 
