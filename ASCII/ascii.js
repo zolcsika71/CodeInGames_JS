d42b41a7c445316e4b67715ff80cb99fe85a3f70
zolcsika71 <bushdoctor2008@gmail.com>
2020-03-02 16:44:20 +0100

    classes

--- ASCII/ascii.js
+++ ASCII/ascii.js
@@ -4,7 +4,8 @@ const
     TEXT = readline(),
     ABC_LENGTH = 27,
     abcId = {
-        A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J: 9, K: 10, L: 11, M: 12, N: 13, O: 14, P: 15, Q: 16, R: 17, S: 18, T: 19, U: 20, V: 21, W: 22, X: 23, Y: 24, Z: 25
+        A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8, J: 9, K: 10, L: 11, M: 12, N: 13, O: 14, P: 15, Q: 16, R: 17, S: 18, T: 19, U: 20, V: 21, W: 22, X: 23, Y: 24, Z: 25,
+        a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9, k: 10, l: 11, m: 12, n: 13, o: 14, p: 15, q: 16, r: 17, s: 18, t: 19, u: 20, v: 21, w: 22, x: 23, y: 24, z: 25
     };
 
 let letters = [],
@@ -75,7 +76,7 @@ function printRows(rows) {
 
 
 createABC(ABC);
-testInput(ABC, L_WIDTH, L_HEIGHT, TEXT);
+//testInput(ABC, L_WIDTH, L_HEIGHT, TEXT);
 createLetters(letters);
 initRows(rows);
 createRows(rows, letters, abcId, TEXT);
