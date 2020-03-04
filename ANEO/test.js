0e2f4e04e5dbe07dc8b10a368a697eb5e7347710
zolcsika71 <bushdoctor2008@gmail.com>
2020-03-04 17:15:13 +0100

    aneo

--- ANEO/test.js
+++ ANEO/test.js
@@ -13,13 +13,12 @@ let trafficLights = [];
 
 for (let i = 0; i < lightCount; i++) {
 
-        let distance = dist[i],
-            duration = dur[i];
+        let distance = dist[i] / 1000,
+            duration = dur[i] / 3600;
 
         trafficLight = {
             distance: distance,
-            duration: duration,
-            state: true // means green
+            duration: duration
         };
 
     trafficLights.push(trafficLight);
@@ -36,14 +35,14 @@ function printInput() {
 }
 
 function convertKmToMs(value) {
-    return (value * 1000 / 3600).toFixed(2);
+    return value * 1000 / 3600
 }
 
-function passTrafficLight(trafficLight, speedMs) {
-    let secondsToLight = trafficLight.distance / speedMs,
+function passTrafficLight(trafficLight, speed) {
+    let secondsToLight = trafficLight.distance / speed,
         period = secondsToLight / trafficLight.duration;
 
-    console.error(`speed: ${speedMs} dist: ${trafficLight.distance} duration: ${trafficLight.duration}`);
+    console.error(`speed: ${speed} dist: ${trafficLight.distance} duration: ${trafficLight.duration}`);
     console.error(`secondsToLight: ${secondsToLight} period: ${period}`);
     console.error(`secondsToLight: ${period % 2 === 0} `);
 
@@ -53,13 +52,12 @@ function passTrafficLight(trafficLight, speedMs) {
 function solve(trafficLights, maxSpeed) {
     for (let speed = 1; speed <= maxSpeed; speed++) {
 
-        let speedMs = convertKmToMs(speed),
-            count = 0;
+        let count = 0;
 
         console.error(`current speed: ${speed}`);
 
         for (let trafficLight of trafficLights) {
-            let pass = passTrafficLight(trafficLight, speedMs);
+            let pass = passTrafficLight(trafficLight, speed);
             if (pass)
                 count++;
 
